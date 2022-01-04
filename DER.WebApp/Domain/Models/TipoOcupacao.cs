d95b06aa347c89aa3638ee04ac8d3b481e209694
ClaudioRochaPacheco <claudiopachecodev@gmail.com>
2022-01-04 13:16:31 -0300

    tipoocupacao model

---
+++ DER.WebApp/Domain/Models/TipoOcupacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DER.WebApp.Domain.Models
+{
+    public class TipoOcupacao
+    {
+        public int tipo_ocupacao_id { get; set; }
+        public string nome { get; set; }
+        public double altura_minima { get; set; }
+        public double profundidade_minima { get; set; }
+    }
+}
